625c0f1e21998c856018c915647f56f6a51cbea6
Miguel Fernandez <miguelvfz@outlook.com>
2020-05-13 14:16:32 -0300

    new script powershell for deleting items with condition

---
+++ Scripts/PowerShell/DeleteItemsWithCondition.ps1
@@ -0,0 +1,17 @@
+# 13/05/2020 14:02 executed at Leão prd #
+$site = Get-SPSite http://sharepoint.leaoab.com.br/sites/edme
+$web = $site.OpenWeb()
+$lista = $web.Lists["Vendedores"]
+# Loga quantidade de itens #
+Write-Host "Lista $($lista.Title) tem $($lista.Items.Count) itens"
+$items = $lista.Items
+foreach($item in $items)
+{
+    # Loga quantidade de itens #
+    Write-Host "Lista $($lista.Title) tem $($lista.Items.Count) itens"    
+    # Conditions to not delete #
+    if($item.id -ne 71 && $item.id -ne 25659 && $item.id -ne 72227 && $item.id -ne 73611) 
+    {
+        $lista.GetItemById($item.id).Delete()
+    }
+}
